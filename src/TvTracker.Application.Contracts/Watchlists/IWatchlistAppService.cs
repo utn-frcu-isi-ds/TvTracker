400983bd9832158682daff206b2ec03adb926798
enzoftv <eftanga@gmail.com>
2024-09-13 20:05:32 -0300

    add watchlist

---
+++ src/TvTracker.Application.Contracts/Watchlists/IWatchlistAppService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace TvTracker.Watchlists
+{
+    public interface IWatchlistAppService : IApplicationService
+    {
+        Task AddSerieAsync(int serieId);
+    }
+}
---
+++ src/TvTracker.Domain/Watchlists/Watchlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TvTracker.Series;
+using Volo.Abp.Domain.Entities;
+
+namespace TvTracker.Watchlists
+{
+    public class Watchlist : AggregateRoot<int>
+    {        
+        public List<Serie> Series { get; set; }
+
+        public Watchlist()
+        {
+            Series = new List<Serie>();
+        }
+    }
+}

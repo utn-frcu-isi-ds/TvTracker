774d9de4e971ce92387b9cc93ad321f48bb90609
enzoftv <eftanga@gmail.com>
2024-10-02 20:37:33 -0300

    test: add OmdbService integration test

---
+++ test/TvTracker.EntityFrameworkCore.Tests/EntityFrameworkCore/Domains/Series/EfCoreOmdbService_Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TvTracker.EntityFrameworkCore;
+using Xunit;
+
+namespace TvTracker.Series
+{
+    [Collection(TvTrackerTestConsts.CollectionDefinitionName)]
+    public class EfCoreOmdbService_Tests : OmdbService_Tests<TvTrackerEntityFrameworkCoreTestModule>
+    {
+    }
+}

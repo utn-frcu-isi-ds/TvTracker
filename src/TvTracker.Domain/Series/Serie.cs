7dd41b4befd42c485c7998848ee25e5841e4cd42
enzoftv <eftanga@gmail.com>
2024-10-04 19:07:06 -0300

    add user to serie

--- src/TvTracker.Domain/Series/Serie.cs
+++ src/TvTracker.Domain/Series/Serie.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Auditing;
 using Volo.Abp.Domain.Entities;
 
 namespace TvTracker.Series
 {
-    public class Serie : AggregateRoot<int>
+    public class Serie : AggregateRoot<int>, IMustHaveCreator<Guid>
     {
         public string Title { get; set; }
+
+        public Guid Creator { get; set; }
+
+        public Guid CreatorId { get; set; }
     }
 }
---
+++ src/TvTracker.EntityFrameworkCore/Migrations/20241004213001_add-user-to-serie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TvTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class addusertoserie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Creator",
+                table: "AppSeries",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatorId",
+                table: "AppSeries",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Creator",
+                table: "AppSeries");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "AppSeries");
+        }
+    }
+}
